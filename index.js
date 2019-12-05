889603370a91b637abf29e784f54608f2585d097
Paul B-H <51964537+PaulB-H@users.noreply.github.com>
2019-12-05 16:08:17 -0500

    Fixed PDF generation

--- index.js
+++ index.js
@@ -17,10 +17,24 @@ var questions = [
 
 async function myInquirer(){
   var answers = await inquirer.prompt(questions);
-
   var response = await axios.get(`https://api.github.com/users/${answers.userQuery}`);
+  var myHTML = createHTML(response);
+  createPDF(myHTML);
+};
 
- let myHTML = (`<!DOCTYPE html>
+function createPDF(html){
+  console.log(`we are passing this: ${html}`)
+  var newPdf = pdf.create(html, options).toFile('./businesscard.pdf', function(err, res) {
+    if (err) return console.log(err);
+    console.log(res);
+  });
+  return newPdf;
+};
+
+myInquirer();
+
+function createHTML (res){
+ let myHTML =  (`<!DOCTYPE html>
   <html lang="en">
   <head>
   <meta charset="UTF-8">
@@ -53,86 +67,32 @@ async function myInquirer(){
   }
   </style>
     <div id="content">
-      <h1 id="userName">${response.data.name}</h1>
+      <h1 id="userName">${res.data.name}</h1>
       <div id="profileImg">
-        <img src="${response.data.avatar_url}">
+        <img src="${res.data.avatar_url}">
       </div>
-      <h3>${response.data.bio}</h3>
+      <h3>${res.data.bio}</h3>
       <table>
           <tr>
-              <td style="text-align: center;">Location: ${response.data.location}</td>
+              <td style="text-align: center;">Location: ${res.data.location}</td>
           </tr>
           <tr>
-              <td><li><a href="${response.data.url}">Link to Profile</a></li></td>
-              <td><li><a href="${response.data.blog}">Link to Blog</a></li></td>
+              <td><li><a href="${res.data.url}">Link to Profile</a></li></td>
+              <td><li><a href="${res.data.blog}">Link to Blog</a></li></td>
           </tr>
           <tr>
-              <td>Followers: ${response.data.followers}</td>
-              <td>Following: ${response.data.following}</td>
+              <td>Followers: ${res.data.followers}</td>
+              <td>Following: ${res.data.following}</td>
           </tr>
           <tr>
-            <td>Public Repos: ${response.data.public_repos}</td>
+            <td>Public Repos: ${res.data.public_repos}</td>
           </tr>
       </table> 
     </div>
   </body>
   </html>
     `);
-    generateStuff(myHTML);
-    console.log(myHTML);
-};
 
-async function generateStuff(html){
-  var newHTML = await fs.writeFile('newfile.html', html, function (err) {
-  if (err) throw err;
-  console.log('File is created successfully.');
-  });
-  readAndGenerate();
-  console.log(`File read success`);
-}
-
-async function readAndGenerate(){
-  var newHTML = await fs.readFileSync('./newfile.html', 'utf8');
-  createPDF(newHTML);
-}
-
-async function createPDF(html){
-  console.log(`we are passing this: ${html}`)
-  var newPdf = await pdf.create(html, options).toFile('./businesscard.pdf', function(err, res) {
-    if (err) return console.log(err);
-    console.log(res);
-  });
-
-  return newPdf;
-};
-
-myInquirer();
-
-
-// var myHTML = async function generateStuff(){
-
-  // var html = await fs.writeFile('newfile.html', myHTML, function (err) {
-  //   if (err) throw err;
-  //   console.log('File is created successfully.');
-  // });
-
-//   var newHTML = fs.readFileSync('./newfile.html', 'utf8');
-//   console.log(`File read success, ${newHTML}`);
-
-//   return newHTML;
-
-// };
-  
-// async function createPDF(html){
-//   console.log(`we are passing this: ${html}`)
-//   var newPdf = await pdf.create(html, options).toFile('./businesscard.pdf', function(err, res) {
-//     if (err) return console.log(err);
-//     console.log(res);
-//   });
-
-//   return newPdf;
-// };
-
-//   // createPDF(myHTML);
-
-// });
+    return myHTML;
+    
+  };
